6389dc6c728f1e877d02e71445ea2211555309b5
David Martínez García <126606809+DavidOcacho@users.noreply.github.com>
2023-12-21 18:31:40 +0100

    Finally, launching and moving programs

--- scripts/setup_selector.sh
+++ scripts/setup_selector.sh
@@ -1,11 +1,27 @@
 # Setup selector for displaying different programs depending on the work
+stty -echo
 
 # Function to display menu
-display_menu() {
+function display_menu()
+{
     echo "Select a setup to display:"
     echo "(Spotify, Discord and Google by default)"
     echo "1. C++"
     echo "2. Godot"
+}
+
+function launch_app()
+{
+    $1 &
+    sleep 0.5
+    i3-msg '[class='$2'] move container to workspace '$3'' 
+}
+
+function launch_default_apps()
+{
+    launch_app discord discord 3
+    launch_app google-chrome-stable Google 2
+    launch_app spotify Spotify 3
 }
 
 # Function to perform action 1
@@ -18,12 +34,6 @@ perform_action_1() {
 perform_action_2() {
     echo "Performing Action 2"
     # Add your logic for Action 2 here
-}
-
-# Function to perform action 3
-perform_action_3() {
-    echo "Performing Action 3"
-    # Add your logic for Action 3 here
 }
 
 echo ""
@@ -52,15 +62,10 @@ while true; do
         2)
             perform_action_2
             ;;
-        3)
-            perform_action_3
-            ;;
-        *)
-            echo "Quitting..."
-            break
-            ;;
     esac
 
-    # Add a newline for better readability
+    launch_default_apps
+    break
+
     echo
 done
